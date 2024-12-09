93f0c88eb8cbabada7c71d87e55926d9559bbb4c
Manibingi <bingimaniprasad01@gmail.com>
2024-12-09 20:32:29 +0530

    added login/register routes and jwt tokenization

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const userRoute = require("./routes/user");
+const bodyParser = require("body-parser");
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use("/api/user", userRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is listening to port: ${PORT}`);
